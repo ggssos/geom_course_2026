6d0c08da32e61e5c2f9b136ef3bb9fe13ad88711
Софья Обухова <estdzojs@gmail.com>
2026-09-06 19:29:12 +0300

    Complete homework 0

---
+++ homeworks/hw00/hello.py
@@ -0,0 +1,9 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+
+print("Python:", sys.version)
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Matplotlib:", matplotlib.__version__)
